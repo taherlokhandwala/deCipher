adea726854b84231640c24ab527f9908dafe6a81
taherlokhandwala <taher.mar1999@gmail.com>
2026-04-01 13:11:29 -0400

    Implemented database setup

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "covers.openlibrary.org" },
       {
         protocol: "https",
-        hostname: "lspfdyhgsrgsxcju.public.blob.vercel-storage.com",
+        hostname: "lsvdfrs5juwi4ubg.public.blob.vercel-storage.com",
       },
     ],
   },
